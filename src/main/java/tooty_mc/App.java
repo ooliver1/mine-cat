dfc343d2bb9cb85a89751d15550030f6932d05fc
ooliver1 <oliverwilkes2006@icloud.com>
2021-10-19 18:46:32 +0100

    Initial commit

---
+++ src/main/java/tooty_mc/App.java
@@ -0,0 +1,13 @@
+package tooty_mc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
