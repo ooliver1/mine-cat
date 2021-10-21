ee35bbbcb137869f1e1f7ae3c429328982a855b5
ooliver1 <oliverwilkes2006@icloud.com>
2021-10-21 16:48:21 +0100

    ChatMessage to get player chat messages

--- src/main/java/tootymc/Tooty.java
+++ src/main/java/tootymc/Tooty.java
@@ -2,16 +2,17 @@ package tootymc;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class App extends JavaPlugin {
+public class Tooty extends JavaPlugin {
 
     @Override
     public void onEnable() {
         getLogger().info("Tooty is enabling...");
+        new ChatMessage(this);
+        getLogger().info("ChatMessage is enabled!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("Tooty is disabling...");
     }
-
 }
