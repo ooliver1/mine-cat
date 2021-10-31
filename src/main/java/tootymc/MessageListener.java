d6f044c00072b2c27f5a9f08dada80bf51b33a03
ooliver1 <oliverwilkes2006@icloud.com>
2021-10-31 18:56:11 +0000

    send name in payload for identifying when no link

--- src/main/java/tootymc/MessageListener.java
+++ src/main/java/tootymc/MessageListener.java
@@ -3,6 +3,7 @@ package tootymc;
 import org.bukkit.Server;
 import org.json.JSONObject;
 import java.net.http.WebSocket;
+import org.bukkit.entity.Player;
 import java.util.logging.Logger;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
@@ -29,14 +30,17 @@ public class MessageListener implements Listener {
         JSONObject req = new JSONObject();
         req.put("type", "msg");
         req.put("msg", msg);
-        String uuid = event.getPlayer().getUniqueId().toString();
+        Player player = event.getPlayer();
+        String uuid = player.getUniqueId().toString();
         String id = this.plugin.getDiscordId(uuid);
+        String name = player.getDisplayName();
         if (id != null) {
             req.put("id", id);
             req.put("dc", true);
         } else {
             req.put("id", uuid);
             req.put("dc", false);
+            req.put("name", name);
         }
         client.sendText(req.toString(), true);
     }
